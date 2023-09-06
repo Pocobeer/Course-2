8c9f80ec0f99e226b62151df3b40035c28330a91
Pocobeer <vall.com3454.71@gmail.com>
2023-09-06 16:11:19 +0300

    Update ConsoleApplication1.cpp

--- Саод/лаба1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Саод/лаба1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,12 +3,14 @@
 #include <ctime>
 #include <vector>
 using namespace std;
+
 int main()
 {
 	vector<int> koloda = {0,1,2,3,4,5,6,7,8,9};
 	int i;
 	int index;
 	int j;
+	
 	vector<int> koloda_A;
 	vector<int> koloda_B;
 	srand(time(0));
@@ -33,19 +35,48 @@ int main()
 	for (i = 0; i < koloda_B.size(); i++) {
 		cout << koloda_A[i] << "\t\t" << koloda_B[i] << endl;
 	}
-	for (i = 0; i < koloda.size() + 10; i++) {
-		if (koloda_A[i] > koloda_B[i]) {
-			koloda_A.push_back(koloda_A[i]);
-			koloda_A.push_back(koloda_B[i]);
+	while(koloda_A[0]!=-1 || koloda_B[0] != -1) {
+		if ((koloda_A[0] > koloda_B[0]) || (koloda_A[0] == 0 && koloda_B[0] == 9)) {
+			koloda_A.push_back(koloda_A[0]);
+			koloda_A.push_back(koloda_B[0]);
 			koloda_A.erase(koloda_A.begin());
 			koloda_B.erase(koloda_B.begin());
+			koloda_B.push_back(-1);
+			koloda_B.push_back(-1);
+
 		}
 		else {
-			koloda_B.push_back(koloda_A[i]);
-			koloda_B.push_back(koloda_B[i]);
+			koloda_B.push_back(koloda_A[0]);
+			koloda_B.push_back(koloda_B[0]);
 			koloda_A.erase(koloda_A.begin());
 			koloda_B.erase(koloda_B.begin());
+			koloda_A.push_back(-1);
+			koloda_A.push_back(-1);
 		}
+		cout << koloda_A.size() << "bebe" << koloda_B.size() << endl;
+		/*int a = 0, b = 0, c = 0, d = 0;
+		while (b < koloda_A.size()) {
+			if (koloda_A[b] != -1) {
+				koloda_A[a] = koloda_A[b];
+				b++;
+			}
+			b++;
+		}
+		while (a < koloda_A.size()) {
+			koloda_A[a] = -1;
+			a++;
+		}
+		while (d < koloda_B.size()) {
+			if (koloda_B[d] != -1) {
+				koloda_B[c] = koloda_B[d];
+				d++;
+			}
+			d++;
+		}
+		while (c < koloda_B.size()) {
+			koloda_B[c] = -1;
+			c++;
+		}*/
 		if (koloda_A.size() > koloda_B.size()) {
 			cout << "Koloda A:\t" << "Koloda B" << endl;
 			for (i = 0; i < koloda_A.size(); i++) {
@@ -58,5 +89,7 @@ int main()
 				cout << koloda_A[i] << "\t\t" << koloda_B[i] << endl;
 			}
 		}
+		cout << koloda_A.size() << "bebe" << koloda_B.size() << endl;
+		if (koloda_A[0] == -1 || koloda_B[0] == -1) break;
 	}
 }
