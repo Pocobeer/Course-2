524f571a3d73862e585073040afabd0f7fbbf7cc
Pocobeer <vall.com3454.71@gmail.com>
2023-09-07 11:41:11 +0300

    1 block

---
+++ Саод/лаба1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int calculating_expression(string expression, int& index) {
+	if (index > expression.length()) {
+		return 0;
+	}
+	char Char = expression[index];
+	index++;
+	if (Char == '+' || Char == '-' || Char == '*' || Char == '/') {
+		int operand1 = calculating_expression(expression, index);
+		int operand2 = calculating_expression(expression, index);
+		if (Char == '+') {
+			return operand1+operand2;
+		}
+		if (Char == '-') {
+			return operand1 - operand2;
+		}
+		if (Char == '*') {
+			return operand1 * operand2;
+		}
+		if (Char == '/') {
+			return operand1 / operand2;
+		}
+	}
+	return Char - '0';
+}
+int main() {
+	string expression;
+	int index = 0;
+	cout << "Enter the expression: " << endl;
+	cin >> expression;
+	int result = calculating_expression(expression, index);
+	cout << "Expression value: " << result << endl;
+}
