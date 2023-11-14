95fc845e6de6b724b5036a9530c1bb251bd8cb4b
Pocobeer <vall.com3454.71@gmail.com>
2023-11-14 20:08:33 +0300

    3

---
+++ Саод/лаба3/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int A, B;
+    cout << "Enter A: " << endl;
+    cin >> A;
+    cout << "Enter B: " << endl;
+    bool sravn = false;
+    if (A > B) sravn = true;
+    int count = 0;
+    for (int i = -1000; i <= 1000; i++) {
+        if (sravn == true) {
+            if (A > i) count++;
+        }
+        else {
+            if (B > i) count++;
+        }
+    }
+    cout << "Serial number = " << count << endl;
+}
+
