43db7de186d2a293d2d4d5cfb74a5a840fda1d20
Pocobeer <vall.com3454.71@gmail.com>
2024-01-29 21:49:29 +0300

    Update ConsoleApplication1.cpp

--- ООП/лаба6/ConsoleApplication1/ConsoleApplication1.cpp
+++ ООП/лаба6/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,24 +1,64 @@
 ﻿#include <iostream>
+#include<string>
 using namespace std;
 class Family {
-public:
-	void Set_Name(string name) {
-		this->Name = name;
-	}
-	void Set_Surname()
 private:
-	string Name;
-	string Surname;
-	string Otchestvo;
+	string Name, Surname,Fathername;
 	int Age;
-	int Sex;
-	int Status;
+	string Sex;
+	string Status;
+public:
+	void Print();
+	Family(string Surnamev, string Namev, string Fathernamev, int Agev, string Sexv, string Statusv ) {
+		Surname = Surnamev;
+		Name = Namev;
+		Fathername = Fathernamev;
+		Age = Agev;
+		Sex = Sexv;
+		Status = Statusv;
+	}
+	Family(int Agevv) {
+		Age = Agevv;
+	}
+	void setAge(int Age) {
+		this->Age;
+	}
+	void showage() const {
+		cout << "Age: " << Age << endl;
+	}
+	class Salary {
+	private:
+		int salary;
+	public:
+		/*Salary(int salary) {
+			this->salary;
+		}*/
+		int GetSalary() {
+			return salary;
+		}
+		int SetSalary(int salaryv) {
+			salary=salaryv;
+			return salary;
+		}
+	};
+	void Print1() {
+		Salary amount;
+		amount.SetSalary(2000);
+		cout << "Surname: " << Surname << "\t\tName: " << Name << "\t\tFathername: " 
+			<< Fathername << "\nAge: " << Age << "\nSex: " << Sex << "\nStatus: " << Status 
+			<< "\nSalary: " ;
+		int result = amount.GetSalary();
+		cout << result << endl;
+	}
 
 };
+/*void Family::Print() {
+	cout << "Surname: " << Surname << "\t\tName: " << Name << "\t\tFathername: " << Fathername << "\nAge: " << Age << "\nSex: " << Sex << "\nStatus: " << Status << "\nSalary: " << endl;
+}*/
 int main()
 {
-	Family Dad;
-	Dad.Set_Name("Sergey");
-
-
+	Family Me( "Ivanov", "Vladimir", "Sergeevich", 19, "Male", "Student");
+	Me.Print1();
+	const Family const_Family(19);
+	const_Family.showage();
 }
