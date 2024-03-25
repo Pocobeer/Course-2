9bf6e0b3f8f8a265be6e79a7dad3590a64592400
Pocobeer <vall.com3454.71@gmail.com>
2024-03-25 11:52:13 +0300

    ампср

---
+++ теорвер проект/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,46 @@
+import random
+
+n = int(input("Enter the total number of numbers n: "))
+m = int(input("Enter the number of histogram intervals m: "))
+
+# Create lists to store histogram parameters xi and P(xi)
+xi = [10.3, 11.6, 12.9, 14.2, 15.5, 16.8, 18.1]
+Px = [0.1, 0.15, 0.2, 0.15, 0.1, 0.2, 0.1]
+ki = []
+# Input values of xi and P(xi) for each histogram interval
+#for i in range(m):
+#    x = float(input(f"Enter the value of xi for interval {i+1}: "))
+#    p = float(input(f"Enter the value of P(xi) for interval {i+1}: "))
+#    xi.append(x)
+#    Px.append(p)
+for i in range(1,m):
+    k = int(n*Px[i]*(xi[i]-xi[i-1]))
+    ki.append(k)
+l=1
+def RND():
+    return random.random()
+for i in range(1,m):
+    random_value = RND()
+    #for j in range(1,ki[i]):
+        #A_l = xi[i] + random_value*(xi[i]-xi[i-1])
+        #l = l+1
+for i in range(1,l):
+    rand_val = RND()
+    p_1=rand_val(n-1)+1
+    p_2=rand_val(n-1)+1
+    if p_1 != p_2:
+        b = A_l[p_1]
+        A_l[p_1]=A_l[p_2]
+        A_l[p_2] = b
+
+for i in range(1,n):
+    print(A_l[i])
+        
+        
+    
+print("Total number of numbers n:", n)
+print("Number of histogram intervals m:", m)
+print("Histogram parameters xi:", xi)
+print("Histogram parameters P(xi):", Px)
+print("Values of ki:", ki)
+#print("Value of A_1:", A_1)
