83b8a3730eba7088b1e33235d1aa08e56cd0116d
Pocobeer <vall.com3454.71@gmail.com>
2024-04-12 14:42:40 +0300

    о

--- Эвм/lab_1/lab_1.asm
+++ Эвм/lab_1/lab_1.asm
@@ -4,40 +4,22 @@ sseg segment stack 'stack'
 sseg ends
 ; Определение данных
 data segment
-x dw -5
-zerx db ?
-neg_x db ?
-posx db ?
-msg1 db 10, 13, 'input number', '$'
+;zerx db ?
+;neg_x db ?
+;posx db ?
+;msg1 db 10, 13, 'input number', '$'
 msg2 db 10, 13, 'Number is positive', '$'
 msg3 db 10, 13, 'Number is negative', '$'
 msg4 db 10, 13, 'Number is null', '$'
 data ends
 
 ; Макроопределения
-check_num macro num
-local _neg, _pos, _zer, myend
+check_num macro num, _neg, _pos, _zer
     cmp  num, 0
     je _zer
     jg _pos
     jl _neg
 
-    _neg:
-        mov bx, num
-        lea dx, msg3
-        call print_msg
-        jmp myend
-    _pos:
-        mov cx, num
-        lea dx, msg2
-        call print_msg
-        jmp myend
-    _zer:
-        mov dx, 1
-        lea dx, msg4
-        call print_msg
-        jmp myend
-    myend:
 
 endm
 
@@ -46,9 +28,26 @@ assume cs:code,ds:data,ss:sseg
 start: mov ax,data ; настроить сегментный
  mov ds,ax ; регистр DS на данные
  .386
- check_num x
- mov ah,4ch ; завершить программу
- int 21h ; и выйти в DOS
+ mov ax,0
+ check_num ax, m1, m2, m3
+  m1:
+        mov bx, ax
+        lea dx, msg3
+        call print_msg
+        jmp myend
+    m2:
+        mov cx, ax
+        lea dx, msg2
+        call print_msg
+        jmp myend
+    m3:
+        mov dx, 1
+        lea dx, msg4
+        call print_msg
+        jmp myend
+    myend:
+        mov ah,4ch ; завершить программу
+        int 21h ; и выйти в DOS
 
 print_msg proc
  push ax 
