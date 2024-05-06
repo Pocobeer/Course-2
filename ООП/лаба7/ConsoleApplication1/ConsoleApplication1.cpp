8cf46d5930d368ea716beb71d1312b9a14a3d9f7
Pocobeer <vall.com3454.71@gmail.com>
2024-05-06 16:36:19 +0300

    ж

---
+++ ООП/лаба7/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+class Word {
+	string word;
+public:
+	Word(string w) :word(w) {};
+	void inputWord() {
+		cout << "Enter word: ";
+		cin >> word;
+	}
+	static int count;
+	static int Get_Count() {
+		return count;
+	}
+	void showWord() {
+	cout << word << endl;
+	}
+};
+int Word::count = 0;
+class WordCount : public Word {
+	int wordCount;
+public:
+	WordCount(string w) :Word(w) {
+		wordCount = 1;
+		for (int i = 0; i < w.length(); i++) {
+			if (w[i] == ' ') {
+				wordCount++;
+			}
+		}
+	}
+	void showWordCount() {
+		showWord();
+		cout << wordCount << endl;
+	}
+	static void Set_Count() {
+		count = 0;
+	}
+};
+
+int main() {
+	Word w_1("Pupupu");
+	w_1.showWord();
+	cout << "type of class:" << typeid(w_1).name() << endl;
+	WordCount w_2("Pupupupu pupupu");
+	cout << "type of class:" << typeid(w_1).name() << endl;
+	w_2.showWordCount();
+	cout << "type of class:" << typeid(w_2).name() << endl;
+	
+
+}
