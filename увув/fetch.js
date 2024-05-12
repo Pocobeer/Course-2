c79b66795fcf02fef9a29b52db76f6bbf18c82a2
Pocobeer <vall.com3454.71@gmail.com>
2024-05-12 14:24:05 +0300

    w

--- увув/fetch.js
+++ увув/fetch.js
@@ -3,24 +3,41 @@ function sendRequest(method, url, body = null) {
     const headers = {
         'Content-Type': 'application/json'
     }
-    return fetch(url, {
-        method: method,
-        body: JSON.stringify(body),
-        headers: headers
-    }).then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        return response.json().then(err => {
-            const error = new Error('Что-то пошло не так');
-            error.data = err;
-            throw error;
+    if (method === 'GET') {
+        return fetch(url, {
+            method: method,
+            headers: headers
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            return response.json().then(err => {
+                const error = new Error('Что-то пошло не так');
+                error.data = err;
+                throw error;
+            });
+        });
+    }
+    else{
+        return fetch(url, {
+            method: method,
+            body: JSON.stringify(body),
+            headers: headers
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            return response.json().then(err => {
+                const error = new Error('Что-то пошло не так');
+                error.data = err;
+                throw error;
+            })
         })
-    })
+    }
 }
-// sendRequest('GET', api_url).
-// then(data => console.log(data)).
-// catch(err => console.log(err));
 sendRequest('POST', api_url, { name: 'Alex' , email: 'alex@ya.ru'}).
+then(data => console.log(data)).
+catch(err => console.log(err));
+sendRequest('GET', api_url).
 then(data => console.log(data)).
 catch(err => console.log(err));
--- увув/post.js
+++ увув/post.js
@@ -1,26 +1,38 @@
-const api_url = 'https://jsonplaceholder.typicode.com/users';
 function sendRequest(method, url, body = null) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open(method, url);
         xhr.responseType = 'json';
-        xhr.setRequestHeader('Content-Type', 'application/json');
+
         xhr.onload = () => {
-            if(xhr.status >= 400) {
+            if (xhr.status >= 400) {
                 reject(xhr.response);
             } else {
                 resolve(xhr.response);
             }
-        }
+        };
+
         xhr.onerror = () => {
             reject(xhr.response);
+        };
+
+        if (method === 'GET') {
+            xhr.send();
+        } else {
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify(body));
         }
-        xhr.send(JSON.stringify(body));
     });
 }
-// sendRequest('GET', api_url).
-// then(data => console.log(data)).
-// catch(err => console.log(err));
-sendRequest('POST', api_url, { name: 'Alex' , email: 'alex@ya.ru'}).
-then(data => console.log(data)).
-catch(err => console.log(err));
+
+const api_url = 'https://jsonplaceholder.typicode.com/users';
+
+// Пример использования функции для отправки GET-запроса
+sendRequest('GET', api_url)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+// Пример использования функции для отправки POST-запроса
+sendRequest('POST', api_url, { name: 'Alex', email: 'alex@ya.ru' })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
